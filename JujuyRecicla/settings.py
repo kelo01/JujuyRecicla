8e3bbc0c53b74475ed0772aaff80e44f2e2d1de4
kelo01 <118781340+kelo01@users.noreply.github.com>
2024-12-03 07:40:54 -0300

    a

--- JujuyRecicla/settings.py
+++ JujuyRecicla/settings.py
@@ -137,4 +137,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CSRF_TRUSTED_ORIGINS=['http://*','https://web-production-4ece.up.railway.app']
